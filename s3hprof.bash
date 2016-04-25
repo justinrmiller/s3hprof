3b49341fbba99ede7384af5c27e3d849670902aa
Justin Miller <justin.miller@protectwise.com>
2016-04-25 13:31:41 -0600

    Changing file change detection, adding set -e, making a few parameters overridable.

--- s3hprof.bash
+++ s3hprof.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -e
+
 S3_BUCKET=<insert bucket here>
+
 HPROF_S3_FOLDER=hprofs
-HPROF_LOCAL_DIR=/tmp
+LOCAL_DIR=${HPROFS_LOCAL_DIR:-/tmp}
+REGEX=${HPROFS_REGEX:-".*hprof.*"}
+
+echo "Using local dir: $LOCAL_DIR"
+echo "Using regex: $REGEX"
 
 COMMAND=$1
 
@@ -16,7 +23,7 @@ SERVER_SIDE_ENCRYPTION_AWS_CLI="--sse AES256"
 #
 # ./s3hprof.bash list <hostname> - This lists all hprofs that s3hprof can retrieve for you
 # ./s3hprof.bash get <hostname> <filename> - This retrieves the file filename for host hostname
-# ./s3hprof.bash check-then-upload - This command will check HPROF_LOCAL_DIR for new hprofs that
+# ./s3hprof.bash check-then-upload - This command will check LOCAL_DIR for new hprofs that
 # are finished and upload them (eventually this will also remove them locally, at least optionally)
 # ./s3hprof.bash all-hprofs - This command will list all available hprofs (all servers/hostnames)
 # ./s3hprof.bash all-hprofs-html - This command will list all available hprofs (all servers/hostnames)
@@ -47,21 +54,26 @@ fi
 # handle uploading hprofs if their PIDs no longer exist, will upload then delete the hprof
 # (this script works off the assumption that the process dies when an hprof is done)
 if [ "$COMMAND" == "check-then-upload" ] && [ "$#" -eq 1 ]; then
-  echo "Checking $HPROF_LOCAL_DIR for hprofs to upload..."
-  for f in $HPROF_LOCAL_DIR/*.hprof
+  echo "Checking $LOCAL_DIR for hprofs to upload..."
+  declare -a files_to_upload
+  i=0
+  cd $LOCAL_DIR
+  IFS=$'\n'; for f in $( find . -maxdepth 1 -type f -mmin +0 -regex "./${REGEX}" -print )
   do
-    echo "Checking if process is still running for $f"
-    PID="${f//[^0123456789]/}"
-    if ps -p $PID > /dev/null 2>&1; then
-      echo "$PID is running, skipping in an attempt to get the full hprof next time"
-    else
-      echo "$PID is not running, harvesting hprof..."
-      timestamp=$(date +%s)
-      aws s3 cp $HPROF_LOCAL_DIR/java_pid$PID.hprof s3://$S3_BUCKET/$HPROF_S3_FOLDER/$HOSTNAME/$timestamp/java_pid$PID.hprof $SERVER_SIDE_ENCRYPTION_AWS_CLI
-      echo "Cleaning up java_pid$PID.hprof"
-      # do this when we've tested things a bit more
-    fi
+    files_to_upload[$i]="$f"
+    # Evaluates to false when i==0, thanks bash!
+    ((i++)) || true
   done
+
+  if [ ${#files_to_upload[@] -gt 0} ]; then
+    echo "Uploading files one at a time:"
+    TS=$(date +%s)
+    for f in ${files_to_upload[@]}
+    do
+      aws s3 cp $f "s3://$S3_BUCKET/$HPROF_S3_FOLDER/$HOSTNAME/$TS/"
+      rm $f
+    done
+  fi
 elif [ "$COMMAND" == "check-then-upload" ]; then
   echo "Usage: s3hprof.bash check-then-upload"
 fi
